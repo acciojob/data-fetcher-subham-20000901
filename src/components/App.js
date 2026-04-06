335125088055feed9fc9b83bb9ac38eaae498ae6
subham-20000901 <sinhasubh2000@gmail.com>
2026-04-06 13:20:31 +0530

    fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useState,useEffect} from "react";
+
 
 const App = () => {
+  const API_KEY = "https://dummyjson.com/products"
+  const [products,setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(API_KEY)
+    .then((res) => res.json())
+    .then((data) => {
+      setProducts(data.products);
+    });
+  
+  },[]);
+ 
   return (
     <div>
-        {/* Do not remove the main div */}
+       {products.map((product,index) => (
+        <pre key={index}>{JSON.stringify(product, null, 2)}</pre>
+       ))}
     </div>
   )
 }
 
-export default App
+export default App;
